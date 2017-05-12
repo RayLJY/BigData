97fa2f8e8c08fe2d30e53f7a89221d771463e0c8
Ray <lijianyu@breadtrip.com>
2017-05-12 19:00:21 +0800

    update

---
+++ src/test/java/Ray/test/tool/HttpRequest.java
@@ -0,0 +1,244 @@
+package Ray.test.tool;
+
+import java.io.*;
+import java.net.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * Created by Ray on 17/5/12.
+ * Analog Http Request.
+ */
+public class HttpRequest {
+
+    private String charset = "utf-8";
+    private Integer connectTimeout = null;
+    private Integer socketTimeout = null;
+    private String proxyHost = null;
+    private Integer proxyPort = null;
+
+    public String doGet(String url) throws Exception {
+
+        URL localURL = new URL(url);
+
+        URLConnection connection = this.openConnection(localURL);
+        HttpURLConnection httpURLConnection = (HttpURLConnection) connection;
+
+        httpURLConnection.setRequestProperty("Accept-Charset", charset);
+        httpURLConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+        InputStream inputStream = null;
+        InputStreamReader inputStreamReader = null;
+        BufferedReader reader = null;
+        StringBuffer resultBuffer = new StringBuffer();
+        String tempLine;
+        //响应失败
+        if (httpURLConnection.getResponseCode() >= 300) {
+            throw new Exception("HTTP Request is not success, Response code is " + httpURLConnection.getResponseCode());
+        }
+
+        try {
+            inputStream = httpURLConnection.getInputStream();
+            inputStreamReader = new InputStreamReader(inputStream);
+            reader = new BufferedReader(inputStreamReader);
+
+            while ((tempLine = reader.readLine()) != null) {
+                resultBuffer.append(tempLine);
+            }
+
+        } finally {
+
+            if (reader != null) {
+                reader.close();
+            }
+
+            if (inputStreamReader != null) {
+                inputStreamReader.close();
+            }
+
+            if (inputStream != null) {
+                inputStream.close();
+            }
+
+        }
+
+        return resultBuffer.toString();
+    }
+
+    /**
+     * Do POST request
+     */
+    public String doPost(String url, Map parameterMap) throws Exception {
+
+        /* Translate parameter map to parameter date string */
+        StringBuffer parameterBuffer = new StringBuffer();
+        if (parameterMap != null) {
+            Iterator iterator = parameterMap.keySet().iterator();
+            String key = null;
+            String value = null;
+            while (iterator.hasNext()) {
+                key = (String) iterator.next();
+                if (parameterMap.get(key) != null) {
+                    value = (String) parameterMap.get(key);
+                } else {
+                    value = "";
+                }
+
+                parameterBuffer.append(key).append("=").append(value);
+                if (iterator.hasNext()) {
+                    parameterBuffer.append("&");
+                }
+            }
+        }
+
+        System.out.println("POST parameter : " + parameterBuffer.toString());
+
+        URL localURL = new URL(url);
+
+        URLConnection connection = this.openConnection(localURL);
+        HttpURLConnection httpURLConnection = (HttpURLConnection) connection;
+
+        httpURLConnection.setDoOutput(true);
+        httpURLConnection.setRequestMethod("POST");
+        httpURLConnection.setRequestProperty("Accept-Charset", charset);
+        httpURLConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        httpURLConnection.setRequestProperty("Content-Length", String.valueOf(parameterBuffer.length()));
+
+        OutputStream outputStream = null;
+        OutputStreamWriter outputStreamWriter = null;
+        InputStream inputStream = null;
+        InputStreamReader inputStreamReader = null;
+        BufferedReader reader = null;
+        StringBuffer resultBuffer = new StringBuffer();
+        String tempLine;
+
+        try {
+            outputStream = httpURLConnection.getOutputStream();
+            outputStreamWriter = new OutputStreamWriter(outputStream);
+
+            outputStreamWriter.write(parameterBuffer.toString());
+            outputStreamWriter.flush();
+            //响应失败
+            if (httpURLConnection.getResponseCode() >= 300) {
+                throw new Exception("HTTP Request is not success, Response code is " + httpURLConnection.getResponseCode());
+            }
+            //接收响应流
+            inputStream = httpURLConnection.getInputStream();
+            inputStreamReader = new InputStreamReader(inputStream);
+            reader = new BufferedReader(inputStreamReader);
+
+            while ((tempLine = reader.readLine()) != null) {
+                resultBuffer.append(tempLine);
+            }
+
+        } finally {
+
+            if (outputStreamWriter != null) {
+                outputStreamWriter.close();
+            }
+
+            if (outputStream != null) {
+                outputStream.close();
+            }
+
+            if (reader != null) {
+                reader.close();
+            }
+
+            if (inputStreamReader != null) {
+                inputStreamReader.close();
+            }
+
+            if (inputStream != null) {
+                inputStream.close();
+            }
+
+        }
+
+        return resultBuffer.toString();
+    }
+
+    private URLConnection openConnection(URL localURL) throws IOException {
+        URLConnection connection;
+        if (proxyHost != null && proxyPort != null) {
+            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, proxyPort));
+            connection = localURL.openConnection(proxy);
+        } else {
+            connection = localURL.openConnection();
+        }
+        return connection;
+    }
+
+    /**
+     * Render request according setting
+     */
+    private void renderRequest(URLConnection connection) {
+
+        if (connectTimeout != null) {
+            connection.setConnectTimeout(connectTimeout);
+        }
+
+        if (socketTimeout != null) {
+            connection.setReadTimeout(socketTimeout);
+        }
+
+    }
+
+    /**
+     * Getter & Setter
+     */
+    public Integer getConnectTimeout() {
+        return connectTimeout;
+    }
+
+    public void setConnectTimeout(Integer connectTimeout) {
+        this.connectTimeout = connectTimeout;
+    }
+
+    public Integer getSocketTimeout() {
+        return socketTimeout;
+    }
+
+    public void setSocketTimeout(Integer socketTimeout) {
+        this.socketTimeout = socketTimeout;
+    }
+
+    public String getProxyHost() {
+        return proxyHost;
+    }
+
+    public void setProxyHost(String proxyHost) {
+        this.proxyHost = proxyHost;
+    }
+
+    public Integer getProxyPort() {
+        return proxyPort;
+    }
+
+    public void setProxyPort(Integer proxyPort) {
+        this.proxyPort = proxyPort;
+    }
+
+    public String getCharset() {
+        return charset;
+    }
+
+    public void setCharset(String charset) {
+        this.charset = charset;
+    }
+
+    public static void main(String[] args) {
+      /* Post Request */
+        Map<String, String> dataMap = new HashMap<>();
+        //dataMap.put("username", "Nick");
+        //dataMap.put("a", "v");
+        try {
+            //System.out.println(new HttpRequest().doPost("http://localhost:8081/TestProgect2/LoginServlet", dataMap));
+            System.out.println(new HttpRequest().doGet("http://www.baidu.com"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
